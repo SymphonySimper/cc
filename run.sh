b45becf64a2a1dcee3823404245c2b4ffd84dafb
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2023-12-04 23:40:20 +0530

    add shell script to run individual files

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+builds_dir="builds"
+file_path=$(find . -type f -name "*.rs" -not -name "mod.rs" | fzf)
+file_name=$(basename -- "$file_path")
+file_name_without_ext="${file_name%.*}"
+binary_name="$builds_dir/$file_name_without_ext"
+input_file_name="$builds_dir/${file_name_without_ext}.txt"
+
+echo "Compiling $file_path"
+rustc --out-dir "builds" "$file_path"
+
+if [ ! -f "$input_file_name" ]; then
+	echo "Input for $file_path"
+	wl-paste >"$input_file_name"
+fi
+
+echo "Running $binary_name"
+"./$binary_name" <"$input_file_name"
