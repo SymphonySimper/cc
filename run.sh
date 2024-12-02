2602850e430159bf378eec4bdf29db76c771aaad
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-12-02 19:05:09 +0530

    feat(run)!: use file path for binary and input files

--- run.sh
+++ run.sh
@@ -3,18 +3,17 @@
 builds_dir="builds"
 
 if [ -n "$1" ]; then
-  file_path="$1"
+  file_path="${1#./}"
 else
   file_path=$(find . -type f -name "*.rs" -not -name "mod.rs" | fzf)
 fi
 
-file_name=$(basename -- "$file_path")
-file_name_without_ext="${file_name%.*}"
+file_name_without_ext=$(echo "${file_path%.*}" | tr '/' '-')
 binary_name="$builds_dir/$file_name_without_ext"
 input_file_name="$builds_dir/${file_name_without_ext}.txt"
 
 echo "Compiling $file_path"
-rustc --out-dir "builds" "$file_path"
+rustc -o "$binary_name" "$file_path"
 
 if [ ! -f "$input_file_name" ]; then
   echo "Input for $file_path"
