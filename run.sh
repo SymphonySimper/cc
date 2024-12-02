28a2a39196e143d89837faa141172d75b1c45b83
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-12-02 17:10:45 +0530

    feat(run): if file path is passed use instead of fzf

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 builds_dir="builds"
-file_path=$(find . -type f -name "*.rs" -not -name "mod.rs" | fzf)
+
+if [ -n "$1" ]; then
+  file_path="$1"
+else
+  file_path=$(find . -type f -name "*.rs" -not -name "mod.rs" | fzf)
+fi
+
 file_name=$(basename -- "$file_path")
 file_name_without_ext="${file_name%.*}"
 binary_name="$builds_dir/$file_name_without_ext"
@@ -11,8 +17,8 @@ echo "Compiling $file_path"
 rustc --out-dir "builds" "$file_path"
 
 if [ ! -f "$input_file_name" ]; then
-	echo "Input for $file_path"
-	wl-paste | sed '/^$/d' >"$input_file_name"
+  echo "Input for $file_path"
+  wl-paste | sed '/^$/d' >"$input_file_name"
 fi
 
 echo "Running $binary_name"
