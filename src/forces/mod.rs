be1568ced0bdd2472a9d162eca7c496b1ccc7ab9
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-12-18 14:19:02 +0530

    feat(forces)!: de-nest

--- src/forces/mod.rs
+++ src/forces/mod.rs
@@ -1,1 +1,7 @@
-pub mod problems;
+pub mod a;
+pub mod b;
+pub mod c;
+pub mod d;
+pub mod e;
+pub mod f;
+pub mod g;
--- src/forces/problems/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod a;
-pub mod b;
-pub mod c;
-pub mod d;
-pub mod e;
-pub mod f;
-pub mod g;
